085e5d2e91372e2fc08ebeb93df9c202fa842f0d
sarah801 <sarah2017578@miuegypt.edu.eg>
2025-09-29 13:20:49 +0300

    tst1

--- src/GymService.java
+++ src/GymService.java
@@ -1,7 +1,7 @@
 public class GymService implements Service {
     //gym
     public double  getCost() {
-       return 800;
+       return 80;
     }
     public String  getName() {
        return "Gym";
