21fd009f185a90b67d8b96335c74061dce0c5441
sarah801 <sarah2017578@miuegypt.edu.eg>
2025-10-05 14:32:21 +0300

    sss

--- src/GymService.java
+++ src/GymService.java
@@ -4,6 +4,6 @@ public class GymService implements Service {
        return 200;
     }
     public String  getName() {
-       return "Gym Saraaaaaaaaaa ";
+       return "Gym Sara 1233344";
     }
 }
